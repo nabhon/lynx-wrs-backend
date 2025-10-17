6378b94ddec1213ca2918655fdd4753fd1831747
nabhon <163238574+nabhon@users.noreply.github.com>
2025-10-17 20:50:35 +0700

    start proj

---
+++ src/main/java/com/lynx/lynx_wrs/LynxWrsApplication.java
@@ -0,0 +1,13 @@
+package com.lynx.lynx_wrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynxWrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LynxWrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lynx/lynx_wrs/LynxWrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynx.lynx_wrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LynxWrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
